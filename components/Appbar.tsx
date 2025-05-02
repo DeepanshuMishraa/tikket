f696bdcf89c27b6c74747a8ea04afbf02c79c3fd
Deepanshu Mishra <d4deepanshu723@gmail.com>
2025-05-03 03:09:05 +0530

    Added Wallet Connector

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -1,45 +1,62 @@
+'use client';
 import Link from "next/link";
 import TikketIcon from "./icons/ticketIcon";
 import { Button } from "./ui/button";
 import { ArrowUpRight } from "lucide-react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+
+import "@solana/wallet-adapter-react-ui/styles.css";
+import "../app/wallet.css";
+import { clusterApiUrl } from "@solana/web3.js"
+import { useMemo } from "react";
 
 export default function Appbar() {
+
+  const endpoint = clusterApiUrl("devnet");
+  const wallets = useMemo(() => [], []);
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 backdrop-blur-2xl">
-      <div className="max-w-7xl mx-auto flex items-center justify-between p-4">
-        <Link
-          href="/"
-          className="flex items-center gap-2 hover:opacity-80 transition-opacity"
-        >
-          <TikketIcon />
-          <span className="font-semibold text-white">Tikket</span>
-        </Link>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets}>
+        <WalletModalProvider>
+          <nav className="fixed top-0 left-0 right-0 z-50 backdrop-blur-2xl">
+            <div className="max-w-7xl mx-auto flex items-center justify-between p-4">
+              <Link
+                href="/"
+                className="flex items-center gap-2 hover:opacity-80 transition-opacity"
+              >
+                <TikketIcon />
+                <span className="font-semibold text-white">Tikket</span>
+              </Link>
 
-        <div className="flex items-center gap-6">
-          <Link
-            href="/events"
-            className="text-sm text-gray-300 hover:text-white transition-colors flex items-center gap-1"
-          >
-            Explore Events
-            <ArrowUpRight size={16} className="transition-transform group-hover:translate-x-0.5" />
-          </Link>
+              <div className="flex items-center gap-6">
+                <Link
+                  href="/events"
+                  className="text-sm text-gray-300 hover:text-white transition-colors flex items-center gap-1"
+                >
+                  Explore Events
+                  <ArrowUpRight size={16} className="transition-transform group-hover:translate-x-0.5" />
+                </Link>
 
-          <Button
-            variant="outline"
-            size="sm"
-            className="rounded-xl bg-gray-800 border-none hover:bg-gray-900 duration-300 text-gray-100"
-          >
-            Sign in
-          </Button>
-
-          <Button
-            size="sm"
-            className="bg-purple-600 hover:bg-purple-700 text-white"
-          >
-            Connect Wallet
-          </Button>
-        </div>
-      </div>
-    </nav>
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="rounded-xl bg-gray-800 border-none hover:bg-gray-900 duration-300 text-gray-100"
+                >
+                  Sign in
+                </Button>
+                <WalletMultiButton className="!py-0" />
+              </div>
+            </div>
+          </nav>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   )
 }

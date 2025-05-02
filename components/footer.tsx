fdaec99152876266d1f92b4b366157842e79e969
Deepanshu Mishra <d4deepanshu723@gmail.com>
2025-05-03 02:14:06 +0530

    added ui

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="mt-24 px-6 py-4 mx-auto w-full max-w-7xl backdrop-blur-md border-t border-white/10 text-sm text-white">
+    <footer className="px-6 py-4 mx-auto w-full max-w-7xl backdrop-blur-md border-t border-white/10 text-sm text-white">
       <div className="flex items-center justify-between w-full">
         <nav className="flex space-x-6">
           <Link href="/" className="font-semibold opacity-90 hover:opacity-100 transition-opacity">Tikket</Link>
